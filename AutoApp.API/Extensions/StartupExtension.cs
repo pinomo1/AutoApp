cfcd9afb13613f4db1e0df2442e0196f7be6597c
Nail Shamsudinov <nail.shamsudinov@gmail.com>
2026-04-08 11:08:04 +0400

    Set up commenting of (selectively) all the projects in the solution

--- AutoApp.API/Extensions/StartupExtension.cs
+++ AutoApp.API/Extensions/StartupExtension.cs
@@ -1,20 +1,24 @@
-using System.Reflection;
 using Microsoft.OpenApi;
 
 namespace AutoApp.API.Extensions;
 
+/// <summary>
+/// Various extensions for startup
+/// </summary>
 public static class StartupExtension
 {
     extension(IServiceCollection services)
     {
+        /// <summary>
+        /// Adds swagger to the services
+        /// </summary>
         public void AddSwagger()
         {
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AutoApp", Version = "v1" });
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
+                var xmlFiles = Directory.GetFiles(AppContext.BaseDirectory,"*.xml",SearchOption.AllDirectories).ToList();
+                xmlFiles.ForEach(xmlFile => c.IncludeXmlComments(xmlFile)); 
             });
         }
     }
